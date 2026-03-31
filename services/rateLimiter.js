ba9233718ac5beeebdb7baebac44260bde658754
Dvir Levy <dvirlh1@gmail.com>
2026-03-31 08:54:22 +0300

    increase production rate limit and enable deployment workflow

--- services/rateLimiter.js
+++ services/rateLimiter.js
@@ -3,7 +3,7 @@ import { rateLimit } from "express-rate-limit";
 // Rate limiting configuration
 export const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: process.env.NODE_ENV === "development" ? 1000 : 10, // 1000 for dev, 100 for prod
+  max: process.env.NODE_ENV === "development" ? 1000 : 100, // 1000 for dev, 100 for prod
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
   message: "Too many requests from this IP, please try again after 15 minutes",
