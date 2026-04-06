ba6bacc948bad56a7cac5cb7a2ef09276b5aa364
Dvir Levy <dvirlh1@gmail.com>
2026-04-07 00:49:07 +0300

    refactor: update ecosystem configuration to use exports instead of module.exports

--- ecoSystem.config.js
+++ ecoSystem.config.js
@@ -1,12 +1,10 @@
-module.exports = {
-    apps: [
-        {
-            name: "app",
-            script: "server.js",
-            cwd: "/home/ec2-user/portfolio-bot-server",
-            env_production: {
-                NODE_ENV: "production"
-            }
+exports.apps = [
+    {
+        name: "app",
+        script: "server.js",
+        cwd: "/home/ec2-user/portfolio-bot-server",
+        env_production: {
+            NODE_ENV: "production"
         }
-    ]
-};
+    }
+]
