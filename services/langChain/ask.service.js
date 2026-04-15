e6b6da112a0f85f96269c0e3810a29e8438d6e71
Dvir Levy <dvirlh1@gmail.com>
2026-04-15 11:15:53 +0300

    add question translation with openai

--- services/langChain/ask.service.js
+++ services/langChain/ask.service.js
@@ -3,10 +3,31 @@ import { getVectorStore } from "../../utils/vector-store.js";
 import { getSystemPrompt } from "../prompts.js";
 
 export async function ask(question, language = "en-US") {
+    const model = new ChatOpenAI({
+        model: "gpt-4o-mini",
+        apiKey: process.env.OPENAI_API_KEY,
+    });
+    let relevantDocs = []
     const vectorStore = await getVectorStore();
     const retriever = vectorStore.asRetriever(4);
+    //translate the question before invoke
+    if (!language.startsWith("en")) {
+        const translationResponse = await model.invoke([
+            {
+                role: "system",
+                content: "Translate the following user question to English for information retrieval. Only return the translated text.",
+            },
+            {
+                role: "user",
+                content: question,
+            },
+        ])
+        relevantDocs = await retriever.invoke(translationResponse.content)
+    }
+    else {
+        relevantDocs = await retriever.invoke(question);
+    }
 
-    const relevantDocs = await retriever.invoke(question);
 
     const context = relevantDocs.map((doc, index) => {
         return `
@@ -14,10 +35,6 @@ export async function ask(question, language = "en-US") {
         Content:${doc.pageContent}`;
     }).join("\n\n");
 
-    const model = new ChatOpenAI({
-        model: "gpt-4o-mini",
-        apiKey: process.env.OPENAI_API_KEY,
-    });
 
     const response = await model.invoke([
         {
