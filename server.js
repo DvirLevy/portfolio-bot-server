cb934962e866c5d79063ab4f41a2cd1d5c7a1022
Dvir Levy <dvirlh1@gmail.com>
2026-09-22 17:36:46 +0300

    add localhost origin for debugging prod

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ const PORT = process.env.PORT
 
 app.use(limiter);
 logger.info("Server memory stats:\n", { memory: process.memoryUsage(),  });
-app.use(cors({ origin: ["https://dvir-levy.netlify.app", "https://dvir-levy.dev", "https://wwww.dvir-levy.dev"], }));
+app.use(cors({ origin: ["https://dvir-levy.netlify.app", "https://dvir-levy.dev", "https://wwww.dvir-levy.dev", "http://localhost:8080"], }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
